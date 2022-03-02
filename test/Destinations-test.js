1a12cd901e61491d8ad2ee869a94d1df4b314afa
Ronald Head <ronaldlhead@gmail.com>
2022-03-02 10:22:10 -0700

    Set up function unit test

---
+++ test/Destinations-test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import Destinations from '../src/Destinations';
+
+describe('Destinations', () => {
+  it('should be a function', function () {
+    expect(Destinations).to.be.a('function');
+  });
+})
