d06eadc558ced84a8bba8a0f3b45187fe92dde67
Ronald Head <ronaldlhead@gmail.com>
2022-03-02 10:37:19 -0700

    Pass instance of Destinations unit tests (correct commit)

--- test/Destinations-test.js
+++ test/Destinations-test.js
@@ -1,8 +1,15 @@
 import { expect } from 'chai';
 import Destinations from '../src/Destinations';
+import destinationsDataSet from '../src/data/destinations-data';
 
 describe('Destinations', () => {
+  const destinationsTest = new Destinations(destinationsDataSet);
+
   it('should be a function', function () {
     expect(Destinations).to.be.a('function');
+  });
+
+  it("should be a instance of Destinations", function () {
+    expect(destinationsTest).to.be.an.instanceOf(Destinations);
   });
 })
