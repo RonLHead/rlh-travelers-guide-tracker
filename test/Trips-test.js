af56de5da6d8c208403983c8eccf471ec66fd20e
Ronald Head <ronaldlhead@gmail.com>
2022-03-02 11:36:12 -0700

    Pass Trips function unit test

---
+++ test/Trips-test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import Trips from '../src/Trips';
+
+describe('Trips', () => {
+  it('should be a function', function () {
+    expect(Trips).to.be.a('function');
+  });
+});
