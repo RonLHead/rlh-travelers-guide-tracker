738537737e1e5c832035adaaa571e34e56057074
Ronald Head <ronaldlhead@gmail.com>
2022-03-02 10:44:34 -0700

    Pass test to store Destinations objects

--- test/Destinations-test.js
+++ test/Destinations-test.js
@@ -12,4 +12,19 @@ describe('Destinations', () => {
   it("should be a instance of Destinations", function () {
     expect(destinationsTest).to.be.an.instanceOf(Destinations);
   });
+
+  it.only("should store Destinations objects", function () {
+    expect(destinationsTest.destinationsData).to.deep.equal(destinationsDataSet)
+  });
+
+  it("it should be able to find a Destination object by its ID", function () {
+    expect(destinationsTest.findDestination(1)).to.equal({
+        "id": 1,
+        "destination": "Lima, Peru",
+        "estimatedLodgingCostPerDay": 70,
+        "estimatedFlightCostPerPerson": 400,
+        "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+        "alt": "overview of city buildings with a clear sky"
+      })
+  })
 })
