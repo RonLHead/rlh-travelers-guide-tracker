e43dd0edfac1dbf6bd82f4101568b528ecc070be
Ronald Head <ronaldlhead@gmail.com>
2022-03-02 11:42:04 -0700

    Pass Trips instance unit test

--- test/Trips-test.js
+++ test/Trips-test.js
@@ -1,8 +1,15 @@
 import { expect } from 'chai';
 import Trips from '../src/Trips';
+import tripsDataSet from '../src/data/trips-data'
 
 describe('Trips', () => {
+  const tripsTest = new Trips(tripsDataSet);
+
   it('should be a function', function () {
     expect(Trips).to.be.a('function');
+  });
+
+  it("should be a instance of Trips", function () {
+    expect(tripsTest).to.be.an.instanceOf(Trips);
   });
 });
