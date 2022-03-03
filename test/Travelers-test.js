d0135e1c24c6af8abe391be562046da9c2857bc3
Ronald Head <ronaldlhead@gmail.com>
2022-03-03 14:28:31 -0700

    Set up travelers test data set

--- test/Travelers-test.js
+++ test/Travelers-test.js
@@ -1,8 +1,16 @@
 import { expect } from 'chai';
 import Travelers from '../src/Travelers';
+import travelersDataSet from '../src/data/travelers-data';
 
 describe('Travelers', () => {
+
+  let travelers = new Travelers(travelersDataSet);
+
   it("should be a function", function () {
     expect(Travelers).to.be.a('function');
+  });
+
+  it.only("should be a instance of Travelers", function () {
+    expect(travelers).to.be.an.instanceOf(Travelers);
   });
 });
