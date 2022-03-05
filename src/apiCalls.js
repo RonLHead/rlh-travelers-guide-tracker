018481dc2092caf45c865dbb6dbb137bf0ec6846
Ronald Head <ronaldlhead@gmail.com>
2022-03-05 10:57:46 -0700

    Set up apiCalls js

---
+++ src/apiCalls.js
@@ -0,0 +1,17 @@
+//GET requests
+const destinations = fetch("http://localhost:3001/api/v1/destinations	").then((responses) =>
+  responses.json()
+);
+const trips = fetch(
+  "http://localhost:3001/api/v1/trips	"
+).then((responses) => responses.json());
+
+const travelers = fetch("http://localhost:3001/api/v1/travelers	").then((responses) =>
+  responses.json()
+);
+
+export {
+  destinations,
+  trips,
+  travelers
+}
